474718719c5c8f13ad29cd650f627f4adf73f9e1
prinzz9 <joao.prinz@escola.pr.gov.br>
2025-06-24 17:36:50 -0300

    a

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-const cartoes = document.querySelectorAll('.cartao');
+const cards = document.querySelectorAll('.card');
 
-cartoes.forEach(cartao => {
-  cartao.addEventListener('click', () => {
-    cartao.classList.toggle('active');
+for (let i = 0; i < cards.length; i += 2) {
+  const question = cards[i];
+  const answer = cards[i + 1];
+  answer.style.display = 'none';
+
+  question.addEventListener('click', () => {
+    question.style.display = 'none';
+    answer.style.display = 'flex';
   });
-});
+
+  answer.addEventListener('click', () => {
+    answer.style.display = 'none';
+    question.style.display = 'flex';
+  });
+}
